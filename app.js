6eb34320d409ddaecf649feee18f47035d83c216
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-25 11:09:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+const user = {
+  email: 'cc@gmail.com',
+  isActive: true
+};
+
+user.isActive = false;
+console.log(user); // * {email: cc@gmail.com, isActive: false}
+user = {};
+console.log(user); // error bc user is const
